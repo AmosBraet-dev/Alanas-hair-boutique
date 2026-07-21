79fe3bfc6ee30003ef481ab660c7f8db072e91b5
Amos Braet <amosbraet23@gmail.com>
2026-07-21 14:32:02 +0200

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
+  base: '/Alanas-hair-boutique/', 
   plugins: [
     tailwindcss(),
     injectHTML(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
 })
