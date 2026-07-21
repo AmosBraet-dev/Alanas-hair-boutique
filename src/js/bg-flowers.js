8a4cad0a7a4cb43c34804981c9ad6fe409d19c45
Amos Braet <amosbraet23@gmail.com>
2026-07-21 16:24:17 +0200

    fix flower bg

--- src/js/bg-flowers.js
+++ src/js/bg-flowers.js
@@ -7,46 +7,46 @@ const initBgFlowers = () => {
     }
 
     const config = {
-        defaultImage: '/images/bg-flower.png',
+        defaultImage: '/Alanas-hair-boutique/images/bg-flower.png',
         // Beschikbare assets voor de dynamische verlenging
         pool: [
-            '/images/bg-leaf.png',
-            '/images/bg-flowerpart.png',
-            '/images/bg-flowerpart-2.png'
+            '/Alanas-hair-boutique/images/bg-leaf.png',
+            '/Alanas-hair-boutique/images/bg-flowerpart.png',
+            '/Alanas-hair-boutique/images/bg-flowerpart-2.png'
         ]
     };
 
     // 1. Je vaste redactionele basiscompositie (voor de eerste zones)
     const flowerPositions = [
         // === ZONE 1: HERO SECTIE ===
-        { image: '/images/bg-leaf.png', top: '15vh', left: '20%', width: '80px', rotation: '35', opacity: '0.20' },
-        { image: '/images/bg-flowerpart.png', top: '26vh', left: '10%', width: '130px', rotation: '-15', opacity: '0.15' },
-        { image: '/images/bg-leaf.png', top: '38vh', left: '35%', width: '70px', rotation: '75', opacity: '0.06' }, 
-        { image: '/images/bg-flowerpart-2.png', top: '60vh', right: '22%', width: '110px', rotation: '20', opacity: '0.15' },
-        { image: '/images/bg-leaf.png', top: '74vh', right: '12%', width: '90px', rotation: '-60', opacity: '0.2' },
-        { image: '/images/bg-flowerpart.png', top: '88vh', left: '18%', width: '120px', rotation: '140', opacity: '0.14' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '15vh', left: '20%', width: '80px', rotation: '35', opacity: '0.20' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart.png', top: '26vh', left: '10%', width: '130px', rotation: '-15', opacity: '0.15' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '38vh', left: '35%', width: '70px', rotation: '75', opacity: '0.06' }, 
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart-2.png', top: '60vh', right: '22%', width: '110px', rotation: '20', opacity: '0.15' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '74vh', right: '12%', width: '90px', rotation: '-60', opacity: '0.2' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart.png', top: '88vh', left: '18%', width: '120px', rotation: '140', opacity: '0.14' },
 
         // === ZONE 2: DE OVERGANG ===
-        { image: '/images/bg-leaf.png', top: '96vh', left: '28%', width: '90px', rotation: '-40', opacity: '0.20' },
-        { image: '/images/bg-flowerpart-2.png', top: '103vh', left: '72%', width: '130px', rotation: '85', opacity: '0.12' },
-        { image: '/images/bg-flowerpart.png', top: '116vh', left: '38%', width: '140px', rotation: '15', opacity: '0.15' },
-        { image: '/images/bg-leaf.png', top: '123vh', left: '60%', width: '95px', rotation: '-15', opacity: '0.15' },
-        { image: '/images/bg-leaf.png', top: '130vh', left: '44%', width: '75px', rotation: '60', opacity: '0.10' },
-        { image: '/images/bg-flowerpart.png', top: '137vh', left: '68%', width: '120px', rotation: '110', opacity: '0.2' },
-        { image: '/images/bg-flowerpart-2.png', top: '146vh', left: '22%', width: '110px', rotation: '-85', opacity: '0.08' },
-        { image: '/images/bg-leaf.png', top: '154vh', left: '76%', width: '80px', rotation: '45', opacity: '0.16' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '96vh', left: '28%', width: '90px', rotation: '-40', opacity: '0.20' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart-2.png', top: '103vh', left: '72%', width:'130px', rotation: '85', opacity: '0.12' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart.png', top: '116vh', left: '38%', width: '140px', rotation: '15', opacity: '0.15' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '123vh', left: '60%', width: '95px', rotation: '-15', opacity: '0.15' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '130vh', left: '44%', width: '75px', rotation: '60', opacity: '0.10' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart.png', top: '137vh', left: '68%', width: '120px', rotation: '110', opacity: '0.2' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart-2.png', top: '146vh', left: '22%', width: '110px', rotation: '-85', opacity: '0.08' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '154vh', left: '76%', width: '80px', rotation: '45', opacity: '0.16' },
 
         // === ZONE 3: REVIEW / MIDDEN SECTIE ===
-        { image: '/images/bg-flower.png', top: '165vh', left: '-100px', width: '420px', rotation: '60', opacity: '0.15' },
-        { image: '/images/bg-leaf.png', top: '173vh', left: '25%', width: '90px', rotation: '125', opacity: '0.18' },
-        { image: '/images/bg-flowerpart.png', top: '182vh', left: '14%', width: '150px', rotation: '-30', opacity: '0.13' },
-        { image: '/images/bg-leaf.png', top: '191vh', left: '30%', width: '75px', rotation: '15', opacity: '0.16' },
-        { image: '/images/bg-flowerpart-2.png', top: '200vh', left: '16%', width: '130px', rotation: '95', opacity: '0.18' },
-        { image: '/images/bg-leaf.png', top: '209vh', right: '28%', width: '100px', rotation: '-75', opacity: '0.18' },
-        { image: '/images/bg-flower.png', top: '218vh', right: '-160px', width: '550px', rotation: '165', opacity: '0.20' },
-        { image: '/images/bg-flowerpart.png', top: '228vh', right: '15%', width: '140px', rotation: '40', opacity: '0.13' },
-        { image: '/images/bg-leaf.png', top: '238vh', right: '32%', width: '80px', rotation: '150', opacity: '0.16' },
-        { image: '/images/bg-flowerpart-2.png', top: '248vh', right: '45%', width: '120px', rotation: '-10', opacity: '0.18' }
+        { image: '/Alanas-hair-boutique/images/bg-flower.png', top: '165vh', left: '-100px', width: '420px', rotation: '60', opacity: '0.15' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '173vh', left: '25%', width: '90px', rotation: '125', opacity: '0.18' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart.png', top: '182vh', left: '14%', width: '150px', rotation: '-30', opacity: '0.13' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '191vh', left: '30%', width: '75px', rotation: '15', opacity: '0.16' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart-2.png', top: '200vh', left: '16%', width: '130px', rotation: '95', opacity: '0.18' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '209vh', right: '28%', width: '100px', rotation: '-75', opacity: '0.18' },
+        { image: '/Alanas-hair-boutique/images/bg-flower.png', top: '218vh', right: '-160px', width: '550px', rotation: '165', opacity: '0.20' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart.png', top: '228vh', right: '15%', width: '140px', rotation: '40', opacity: '0.13' },
+        { image: '/Alanas-hair-boutique/images/bg-leaf.png', top: '238vh', right: '32%', width: '80px', rotation: '150', opacity: '0.16' },
+        { image: '/Alanas-hair-boutique/images/bg-flowerpart-2.png', top: '248vh', right: '45%', width: '120px', rotation: '-10', opacity: '0.18' }
     ];
 
     // 2. Bereken de totale hoogte van de pagina in pixels én in Viewport Height (vh)
@@ -101,7 +101,7 @@ const initBgFlowers = () => {
     // 4. Grote afsluiter onderaan de pagina (Indien de pagina héél lang is, plaatsen we aan het einde een grote sfeermaker)
     if (totalPageInVh > 3) {
         flowerPositions.push({
-            image: '/images/bg-flower.png',
+            image: '/Alanas-hair-boutique/images/bg-flower.png',
             top: `${(totalPageInVh * 100) - 30}vh`,
             right: '-120px',
             width: '480px',
